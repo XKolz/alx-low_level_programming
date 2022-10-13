7cb90433c8eaca46dc17bce588589fae29f637d5
Samuel <samuelayo61@gmail.com>
2022-10-13 16:59:03 +0000

    Task 0 corrected

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,35 +3,25 @@
 #include <stdarg.h>
 
 /**
- * print_strings - Prints strings, followed by a new line.
- * @separator: The string to be printed between strings.
- * @n: The number of strings passed to the function.
- * @...: A variable number of strings to be printed.
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
  *
- * Description: If separator is NULL, it is not printed.
- *              If one of the strings if NULL, (nil) is printed instead.
+ * Return: sum of its parameters.
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int i;
-	char *str;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
-	{
-		str = va_arg(list, char *);
+		sum += va_arg(list, int);
 
-		if (str)
-			printf("%s", str);
-		else
-			printf("(nil)");
-
-		if ((i < n - 1) && separator)
-			printf("%s", separator);
-	}
-
-	printf("\n");
 	va_end(list);
+
+	return (sum);
 }
